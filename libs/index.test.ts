3a7c5e88f963da0dd06c7046d462ce43bd8d4053
Jiawen Geng <technicalcute@gmail.com>
2021-08-21 11:19:10 +0000

    feat: refine tests

--- libs/index.test.ts
+++ libs/index.test.ts
@@ -1,10 +1,16 @@
-import { getData } from './index'
+import { Urls } from './entity/urls'
+import { getData, initDB } from './index'
 
-it('with cache', async () => {
-  let a = await getData(
-    'https://www.sqlitetutorial.net/sqlite-count-function/',
-    { cache: true }
-  )
+it('test cache', async () => {
+  let url = 'https://www.sqlitetutorial.net/sqlite-count-function/'
+  await initDB()
+  await Urls.delete({ url })
 
-  console.log(a)
+  // cache if not exists
+  let result = await getData(url, { cache: true })
+  expect(result.cache).toBeFalsy()
+
+  // test cache working
+  result = await getData(url, { cache: true })
+  expect(result.cache).toBeTruthy()
 })
